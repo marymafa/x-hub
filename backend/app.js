feec4445339a5edad487d743221816cccb36cb6f
skmsibi <sabelokabelomsibiskm@gmail.com>
2019-04-03 01:00:58 +0200

    added post routes for likes and comments.

--- backend/app.js
+++ backend/app.js
@@ -15,19 +15,34 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 articles(app);
-app.get("/article/likes/:title", (req, res) => {
-    res.json({ title: "req.params", likes: 20 })
+
+app.push("/article/like", (req, res) => {
+    res.send("like successfull!").status(200).end();
 })
-app.get("/video/likes/:title", (req, res) => {
-    res.json({ title: "req.params", likes: 30 })
+app.push("/video/like", (req, res) => {
+    res.send("like successfull!").status(200).end();
 })
 
+app.push("/article/comment", (req, res) => {
+    res.json({ title: "req.params", comments: [{ text: req.body.comment, date: "now" }, { text: "Hi what is osep?", date: "2019-04-02" }, { text: "I am having trouble setting up osep", date: "2019-01-04" }, { text: "well I would love to give you guys a lesson on osep", date: "2019-02-22" }] }).status(200).end();
+})
+app.push("/video/comment", (req, res) => {
+    res.json({ title: "req.params", comments: [{ text: req.body.comment, date: "now" }, { text: "I love this video", date: "2019-04-02" }, { text: "ohhhh amazing stuff guys", date: "2019-01-04" }, { text: "Amazing", date: "2019-02-22" }] }).status(200).end();
+})
+
+app.get("/article/likes/:title", (req, res) => {
+    res.json({ title: "req.params", likes: 20 }).status(200).end();
+});
+app.get("/video/likes/:title", (req, res) => {
+    res.json({ title: "req.params", likes: 30 }).status(200).end();
+});
+
 app.get("/article/comments/:title", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: "Hi what is osep?", date: "2019-04-02" }, { text: "I am having trouble setting up osep", date: "2019-01-04" }, { text: "well I would love to give you guys a lesson on osep", date: "2019-02-22" }] })
-})
+    res.json({ title: "req.params", comments: [{ text: "Hi what is osep?", date: "2019-04-02" }, { text: "I am having trouble setting up osep", date: "2019-01-04" }, { text: "well I would love to give you guys a lesson on osep", date: "2019-02-22" }] }).status(200).end();
+});
 app.get("/video/comments/:title", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: "I love this video", date: "2019-04-02" }, { text: "ohhhh amazing stuff guys", date: "2019-01-04" }, { text: "Amazing", date: "2019-02-22" }] })
-})
+    res.json({ title: "req.params", comments: [{ text: "I love this video", date: "2019-04-02" }, { text: "ohhhh amazing stuff guys", date: "2019-01-04" }, { text: "Amazing", date: "2019-02-22" }] }).status(200).end();
+});
 app.get("/", express.static(path.join(__dirname, "./public")));
 var list = [];
 io.on('connection', function (socket) {
