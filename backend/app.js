61a8efd592e9c39cb205878baa85b565e09be2dc
skmsibi <sabelokabelomsibiskm@gmail.com>
2019-04-02 21:03:41 +0200

    added the retrieve images feature

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,31 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var {articles} = require("./api/articles");
+var { articles } = require("./routes/articles");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+var path = require("path");
+
+app.use(bodyParser.json());
 app.use(bodyParser());
 app.use(cors());
 
 articles(app);
 
 var list = [];
+app.get("/", express.static(path.join(__dirname, "./public")));
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    list.push(msg);
-    io.emit('chat message',list);
-  });
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        list.push(msg);
+        io.emit('chat message', list);
+    });
 });
 
 
 
-http.listen(port, function(){
-  console.log('listening on *:' + 3000);
+http.listen(port, function () {
+    console.log('listening on *:' + 3000);
 });
