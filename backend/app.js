4f9e355db2c1050cef2f0d781bd3397b51785b7c
skmsibi <sabelokabelomsibiskm@gmail.com>
2019-04-02 11:37:21 +0200

    socket feature

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require("path");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 let cors = require("cors");
 var bodyParser = require('body-parser')
 
@@ -10,6 +12,20 @@ var io = require('socket.io')(server);
 app.use(bodyParser());
 app.use(cors());
 
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+    
+    client.on('join', function(data) {
+    	console.log(data);
+    });
+    
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+  
+});
 
 
 server.listen(port, () => {
