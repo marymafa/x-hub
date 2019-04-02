c9e829a47b7d547f49198a0eec0da796e18be813
cindyloppar <cindyloppar@gmail.com>
2019-04-02 16:10:32 +0200

    added a speech bubble

--- client-side/src/components/form.js
+++ client-side/src/components/form.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 
-
 class Form extends Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          value: " "
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: " "
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleChange(event) {
-      this.state({ value: event.target.value })
-  }
+    handleChange(event) {
+        this.state({ value: event.target.value })
+    }
 
-  handleSubmit(event) {
-      event.preventDefault();
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+    }
 
-  render() {
-      return (
-          <form onSubmit={this.handleSubmit} >
-              <label>
-                  
-        <input className='text-input' type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-              <input className="button-submit"type="submit" value="Submit" />
-          </form>
-      );
-  }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} >
+               <div>
+                    <p className="speech-bubble">Speech Bubble</p>
+                </div>
+
+                <label>
+
+                    <input className='text-input' type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input className="button-submit" type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
 
 export default Form;
