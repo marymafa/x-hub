baaf5b7178df6cffc2e0cda3fa413f67a2a1028f
cindyloppar <cindyloppar@gmail.com>
2019-04-03 01:06:55 +0200

    pulled from master

--- client-side/src/routes/index.js
+++ client-side/src/routes/index.js
@@ -7,12 +7,12 @@ export const makeMainRoutes = () => {
   return (
     <Router history={history}>
       <div className="App">
-        {/* <Route
+        <Route
           path="/"
           exact
           strict
-          render={props => <HomePage {...props} />}
-        /> */}
+          render={props => <form {...props} />}
+        />
       </div>
     </Router>
   );
