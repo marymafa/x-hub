6a932be4a5a05d4c6525ff7b980a0152b063e033
cindyloppar <cindyloppar@gmail.com>
2019-04-03 06:19:42 +0200

    separeated routes into different files.

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,13 @@ var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 var express = require("express");
 var { articles } = require("./routes/articles");
+var { chats } = require("./routes/chat");
+var { videos } = require("./routes/videos")
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var path = require("path");
+
 const { Pool, Client } = require('pg')
-// const config = {
-//     user: 'cindy',
-//     database: 'x_hub',
-//     password: 'loppar123',
-//     port: 5432
-// };
 const connectionString = 'postgresql://cindy:loppar123@localhost:5432/x_hub'
 
 app.use(cors());
@@ -31,7 +28,9 @@ const client = new Client({
 })
 client.connect()
 
-articles(app);
+articles(app, client);
+chats(app, client);
+videos(app, client);
 
 app.post("/create/user", async (req, res) => {
     await client.query(`INSERT INTO users(name) VALUES ($1);`, [req.body.name]);
@@ -43,61 +42,7 @@ app.get("/users", async (req, res) => {
     res.send(allUsers.rows).status(200).end();
 })
 
-app.post("/send/message", async (req, res) => {
-    var senderInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.senderName]);
-    var receiverInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.receiverName]);
-    var allUsers = await client.query(`INSERT INTO charts(messages, sender_id, receiver_id) VALUES ($1, $2, $3);`, [req.body.message, senderInfo.rows[0].id, receiverInfo.rows[0].id]);
-    res.status(201).end();
-})
-app.get("/get/messages/:senderName/:receiverName", async (req, res) => {
-    var senderInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.params.senderName]);
-    var receiverInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.params.receiverName]);
-    var messagesFromSender = await client.query(`SELECT * FROM charts where sender_id = $1 and receiver_id= $2;`, [senderInfo.rows[0].id, receiverInfo.rows[0].id]);
-    console.log('messagesFromSender :', messagesFromSender);
-
-})
-
-app.post("/article/like", async (req, res) => {
-    var userInfor = await client.query(`SELECT * FROM public.users WHERE name=$1;`, [req.body.user]);
-    var articleInfo = await client.query(`SELECT * FROM public.articles where title=$1;`, [req.body.title]);
-    if (userInfor.rows[0] && articleInfo.rows[0]) {
-        var res = await client.query(`INSERT INTO articles_likes(liked_articles_id, liked_item)VALUES ($1, $2);`, [userInfor.rows[0].id, articleInfo.rows[0].id]);
-        console.log('res :', res);
-    }
-    // res.send("like successfull!").status(200).end();
-})
-app.post("/video/like", (req, res) => {
-    res.send("like successfull!").status(200).end();
-})
-
-app.post("/article/comment", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: req.body.comment, date: "now" }, { text: "Hi what is osep?", date: "2019-04-02" }, { text: "I am having trouble setting up osep", date: "2019-01-04" }, { text: "well I would love to give you guys a lesson on osep", date: "2019-02-22" }] }).status(200).end();
-})
-app.post("/video/comment", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: req.body.comment, date: "now" }, { text: "I love this video", date: "2019-04-02" }, { text: "ohhhh amazing stuff guys", date: "2019-01-04" }, { text: "Amazing", date: "2019-02-22" }] }).status(200).end();
-})
-
-app.get("/article/likes/:title", (req, res) => {
-
-    // client.query('SELECT * FROM articles_likes;', (err, res) => {
-    //     console.log(err, res)
-    //     client.end()
-    // })
-    res.json({ title: "req.params", likes: 20 }).status(200).end();
-});
-app.get("/video/likes/:title", (req, res) => {
-    res.json({ title: "req.params", likes: 30 }).status(200).end();
-});
-
-app.get("/article/comments/:title", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: "Hi what is osep?", date: "2019-04-02" }, { text: "I am having trouble setting up osep", date: "2019-01-04" }, { text: "well I would love to give you guys a lesson on osep", date: "2019-02-22" }] }).status(200).end();
-});
-app.get("/video/comments/:title", (req, res) => {
-    res.json({ title: "req.params", comments: [{ text: "I love this video", date: "2019-04-02" }, { text: "ohhhh amazing stuff guys", date: "2019-01-04" }, { text: "Amazing", date: "2019-02-22" }] }).status(200).end();
-});
 app.get("/", express.static(path.join(__dirname, "./public")));
-
-
 
 var list = [];
 io.on('connection', function (socket) {
