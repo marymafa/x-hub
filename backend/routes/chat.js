f7fe81fe1c884bc8c7a0588c3b5d1fef9f6e3883
cindyloppar <cindyloppar@gmail.com>
2019-04-03 06:20:25 +0200

    created a file and added routes related to chat.

---
+++ backend/routes/chat.js
@@ -0,0 +1,15 @@
+const chats = (app, client) => {
+    app.post("/send/message", async (req, res) => {
+        var senderInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.senderName]);
+        var receiverInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.receiverName]);
+        var allUsers = await client.query(`INSERT INTO charts(messages, sender_id, receiver_id) VALUES ($1, $2, $3);`, [req.body.message, senderInfo.rows[0].id, receiverInfo.rows[0].id]);
+        res.status(201).end();
+    })
+    app.get("/get/messages/:senderName/:receiverName", async (req, res) => {
+        var senderInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.params.senderName]);
+        var receiverInfo = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.params.receiverName]);
+        var messagesFromSender = await client.query(`SELECT * FROM charts where sender_id = $1 and receiver_id= $2;`, [senderInfo.rows[0].id, receiverInfo.rows[0].id]);
+        console.log('messagesFromSender :', messagesFromSender);
+    })
+};
+module.exports = { chats };
