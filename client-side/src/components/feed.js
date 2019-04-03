90d0e4098e3093c5e3b758a6112ac4850f90b3e3
vuyisile <vuyisilemark@gmail.com>
2019-04-03 23:41:42 +0200

    working on add comment capability

--- client-side/src/components/feed.js
+++ client-side/src/components/feed.js
@@ -11,13 +11,23 @@ const Feeds = props => {
 
   useEffect(() => {
     Axios.get("localhost:3001/article/likes/")
-      .then(function(response) {
+      .then(function (response) {
         console.log(response);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   });
+  function pushToStorage(username, comment, article) {
+    var date = new Date();
+    // var dd = today.getDate();
+    var store = [];
+    var value = store.push({ "user": username, "comment": comment, "date":date });
+
+    localStorage.setItem("comment", JSON.stringify(value))
+
+    return { article: article, comments: value }
+  }
 
   return (
     <div>
