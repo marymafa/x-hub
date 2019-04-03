ce25e1f88d02c500c7fd4a7a6daafaab2b9858ed
moralss <jeramoral@gmail.com>
2019-04-03 02:30:27 +0200

    added styling

--- client-side/src/components/chat-styles.js
+++ client-side/src/components/chat-styles.js
@@ -3,11 +3,17 @@ import styled from "styled-components";
 export const ChatContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr;
+    height:90vh;
 `;
 
 export const UsersWrapper = styled.div`
 //   display: grid;
+    border:1px solid black;
+`;
 
+export const SendSection = styled.div`
+//   display:grid;
+//   just
 `;
 
 export const Send = styled.button`
@@ -17,6 +23,11 @@ export const Send = styled.button`
 
 
 export const ChatWrapper = styled.div`
-//   display: grid;
-
+  display: grid;
+  border:1px solid black;
+  grid-template-rows:7fr 1fr;
+  .send{
+      justifly-self:end;
+      
+  }
 `;
