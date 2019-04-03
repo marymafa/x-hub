8a40648551468c5c7236afcef1c5b718e92eb228
montshejohn <mokgokongjohannes@gmail.com>
2019-04-03 23:39:53 +0200

    update dashboard and comments

--- client-side/src/components/nav.js
+++ client-side/src/components/nav.js
@@ -13,8 +13,8 @@ const Nav = () => {
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-1">Preferences</Dropdown.Item>
             <Dropdown.Item href="#/action-2">X-Capabilities</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">FAQ</Dropdown.Item>
             <Dropdown.Item href="">Bookmarks</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">FAQ</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
         <img src="https://github.com/moralss/x-hub/blob/master/client-side/src/components/X.png?raw=true" />
