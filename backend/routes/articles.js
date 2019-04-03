d41864cca0f008087dd9068d8c14f4c0dd63ad9f
cindyloppar <cindyloppar@gmail.com>
2019-04-03 06:20:06 +0200

    added routes related to articles.

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,4 +1,3 @@
-const formidable = require('formidable');
 var bodyParser = require("body-parser");
 
 var multer = require('multer');
@@ -9,8 +8,41 @@ const upload = multer({
 
 var fs = require("fs");
 var path = require("path");
-const articles = (app) => {
-
+const articles = (app, client) => {
+  app.post("/article", async (req, res) => {
+    var userInfor = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.user]);
+    var articleInfo = await client.query(`INSERT INTO articles(title, user_id) VALUES ($1, $2);`, [req.body.title, userInfor.rows[0].i]);
+    res.send("like successfull!").status(200).end();
+  })
+  app.post("/article/like", async (req, res) => {
+    var userInfor = await client.query(`SELECT * FROM public.users WHERE name=$1;`, [req.body.user]);
+    var articleInfo = await client.query(`SELECT * FROM public.articles where title=$1;`, [req.body.title]);
+    if (userInfor.rows[0] && articleInfo.rows[0]) {
+      var res = await client.query(`INSERT INTO articles_likes(liked_articles_id, liked_item)VALUES ($1, $2);`, [userInfor.rows[0].id, articleInfo.rows[0].id]);
+      res.send("like successfull!").status(200).end();
+    }
+  })
+  app.post("/article/comment", async (req, res) => {
+    console.log('req.body :', req.body);
+    var userInfor = await client.query(`SELECT * FROM users WHERE name=$1;`, [req.body.user]);
+    var articleInfo = await client.query(`SELECT * FROM articles WHERE title=$1;`, [req.body.title]);
+    var res = await client.query(`INSERT INTO public.articles_comments(comment, commenters_id, commented_item) VALUES ( $1, $2, $3);`, [req.body.comment, userInfor.rows[0].id, articleInfo.rows[0].id]);
+    res.send("like successfull!").status(200).end();
+  })
+  app.get("/article/likes/:title", async (req, res) => {
+    var articleInfo = await client.query(`SELECT * FROM articles where title=$1;`, [req.params.title]);
+    var results = await client.query('SELECT * FROM articles_likes WHERE liked_item = $1;', [articleInfo.rows[0].id]);
+    res.json(results).status(200).end();
+  });
+  app.get("/article/comments/:title", async (req, res) => {
+    var articleInfo = await client.query(`SELECT * FROM articles where title=$1;`, [req.params.title]);
+    var articles = await client.query('SELECT * FROM articles_comments where commented_item =$1;', [articleInfo.rows[0].id]);
+    res.json(articles.rows).status(200).end();
+  });
+  app.get("/all/articles", async (req, res) => {
+    var articles = await client.query('SELECT * FROM articles_likes;');
+    res.json(articles.rows)
+  })
 
   app.set('view engine', 'html');
   app.get("/", (req, res) => {
